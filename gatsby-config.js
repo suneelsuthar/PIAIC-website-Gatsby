862c277cd798592dede22dd0fc16ecc4fd8349ec
suneel kumar <suneelsuther123@gmail.com>
2020-01-04 20:40:11 +0500

    Update gatsby-config.js
    revert

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,9 +15,9 @@ module.exports = {
      {
       resolve: `gatsby-source-contentful`,
       options: {
-        spaceId: `ivamkcx9tr1t`,
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
         // Learn about environment variables: https://gatsby.dev/env-vars
-        accessToken: `_LvZZMG0wYI4dtUQ8Z7tubw0DTt95tLh766vvjFy-7w`,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
       },
     },
   ]
